3ee07d066f68874ed7492d1dfed06d854485de16
arthur <arthur.rios007@gmail.com>
2024-02-19 09:40:41 -0300

    chore: add router

---
+++ src/Router.tsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter } from 'react-router-dom'
+import { DefaultLayout } from './layouts/DefautLayout'
+import { Home } from './pages/Home'
+import { Checkout } from './pages/Checkout'
+import { OrderConfirmed } from './pages/OrderConfirmed'
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <DefaultLayout />,
+    children: [
+      { path: '/', element: <Home /> },
+      { path: '/checkout', element: <Checkout /> },
+      { path: '/order-confirmed', element: <OrderConfirmed /> },
+    ],
+  },
+])
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from './App.tsx'
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default.ts'
 import { GlobalStyle } from './styles/global.ts'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './Router.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={defaultTheme}>
-      <App />
+      <RouterProvider router={router} />
       <GlobalStyle />
     </ThemeProvider>
   </React.StrictMode>,
