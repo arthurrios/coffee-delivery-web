b32a39f3889787dc56451d97d8d96d12a118ec0b
arthur <arthur.rios007@gmail.com>
2024-02-22 21:28:05 -0300

    feat: add cart counter

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -1,14 +1,18 @@
 import { ShoppingCart } from '@phosphor-icons/react'
-import { CartContainer } from './styles'
+import { CartContainer, Counter } from './styles'
 import { useTheme } from 'styled-components'
 import { Link } from 'react-router-dom'
+import { useCart } from '../../hooks/useCart'
 
 export function Cart() {
+  const { cart } = useCart()
+
   const { COLORS } = useTheme()
 
   return (
     <Link to="/checkout" style={{ borderRadius: '6px' }}>
       <CartContainer>
+        {cart.length > 0 && <Counter>{cart.length}</Counter>}
         <ShoppingCart size={22} weight="fill" color={COLORS.YELLOW_DARK} />
       </CartContainer>
     </Link>
--- src/components/Cart/styles.ts
+++ src/components/Cart/styles.ts
@@ -1,6 +1,9 @@
 import styled from 'styled-components'
+import { FONTS } from '../../styles/themes/default'
 
 export const CartContainer = styled.div`
+  position: relative;
+
   display: flex;
   align-items: center;
   justify-content: center;
@@ -10,4 +13,21 @@ export const CartContainer = styled.div`
   width: 2.5rem;
   border-radius: 6px;
   background: ${({ theme }) => theme.COLORS.YELLOW_LIGHT};
+`
+export const Counter = styled.div`
+  position: absolute;
+  top: -0.625rem;
+  right: -0.625rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  height: 1.25rem;
+  width: 1.25rem;
+  border-radius: 50%;
+
+  background: ${({ theme }) => theme.COLORS.YELLOW_DARK};
+  color: ${({ theme }) => theme.COLORS.WHITE};
+  ${FONTS.TEXT_XS}
 `
